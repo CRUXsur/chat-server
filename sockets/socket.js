1155f2ed66c5cd96438b4427265f9ca19950988a
CRUXsur <marcojtorrico@hotmail.com>
2022-04-07 15:53:29 -0400

    Chat Server - First Commit

---
+++ sockets/socket.js
@@ -0,0 +1,22 @@
+const { Server } = require('socket.io');
+const {io} = require('../index');
+
+
+//Mensajes de Sockets
+io.on('connection', client =>{
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    // client.on('mensaje', ( payload ) => {
+    //     console.log('Mensaje!!!', payload);
+    //     io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+    // });
+
+    // client.on('emitir-mensaje', ( payload ) => {
+    //     io.emit( 'nuevo-mensaje', 'HEY!!!!!!!!' );
+    // });
+});
+
